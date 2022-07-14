41ca4e44d4a1492043d6dc6c67076386fff4e182
towa-gh <107020935+towa-gh@users.noreply.github.com>
2022-07-14 09:32:30 +0900

    main
    いらないもの削除

---
+++ Apple Game/AppleCleate.h
@@ -0,0 +1,20 @@
+#pragma once
+#pragma once
+class Create {
+private:
+	int SCREEN_HEIGHT = 480;
+
+	int g_Apple;//キャラ画像変数
+	int g_GreenApple;//キャラ画像変数
+	int g_YellowApple;//キャラ画像変数
+	//int g_PoisonApple;//キャラ画像変数
+
+	int ENEMY_MAX = 10;
+
+public:
+	Create();
+	void EnemyControl();
+	int CreateEnemy();
+	void Init();
+};
+extern Create create;
